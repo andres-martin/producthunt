3aed6e5aa6157dbf7d0c4a2569d0855808dc2721
andres-martin <andresmartin266@gmail.com>
2019-04-06 21:33:12 -0500

    new route created and private method added to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,17 @@ class ProductsController < ApplicationController
     def new
         @product = Product.new
     end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to products_path, notice: "Product successfully published"
+        else
+            render :new
+        end       
+    end
+    private
+        def product_params
+            params.require(:product).permit(:name, :url, :description)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/products', to: 'products#index'
+  #get '/products/new', to: 'products#new', as: 'new_product'
+  #post '/products', to: 'products#create'
   resources :products
 
 end
