7b9324f1196dcfc6be999ac3018abdcd9bedbcb5
andres-martin <andresmartin266@gmail.com>
2019-04-07 12:13:51 -0500

    update route and update method created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,15 @@ class ProductsController < ApplicationController
     def edit
         @product = Product.find(params[:id])
     end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            redirect_to products_path, notice: "Product modified successfully"
+        else
+            render :edit
+        end        
+    end
     private
         def product_params
             params.require(:product).permit(:name, :url, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   #post '/products', to: 'products#create'
   #get '/products/:id', to: 'products#show', as: 'product'
   #get '/products/:id/edit', to: 'products#edit'
+  #patch '/products/:id', to: 'products#update'
   resources :products
 
 end
