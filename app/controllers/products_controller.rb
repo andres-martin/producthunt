82dd4b9831a665a403e0969bd1c98a662f0749ff
andres-martin <andresmartin266@gmail.com>
2019-04-07 10:20:42 -0500

    create method show in products_controller.rb and the show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,10 @@ class ProductsController < ApplicationController
             render :new
         end       
     end
+
+    def show
+        @product = Product.find(params[:id])
+    end
     private
         def product_params
             params.require(:product).permit(:name, :url, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get '/products', to: 'products#index'
   #get '/products/new', to: 'products#new', as: 'new_product'
   #post '/products', to: 'products#create'
+  #get '/products/:id', to: 'products#show'
   resources :products
 
 end
