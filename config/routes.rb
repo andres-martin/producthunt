67043154623e1c02214326c3177d732183011e72
andres-martin <andresmartin266@gmail.com>
2019-04-08 22:32:30 -0500

    destroy method created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   #get '/products/:id', to: 'products#show', as: 'product'
   #get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   #patch '/products/:id', to: 'products#update'
+  #delete '/products/:id', to: 'products#destroy'
   resources :products
 
 end
