0f044593dabf5b26f8dc58c77029196e65f1f7bd
andres-martin <andresmartin266@gmail.com>
2019-04-25 13:40:07 -0500

    language translated

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Producthunt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
