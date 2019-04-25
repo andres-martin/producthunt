49b2a83dbb09a7f64712ce9e255b329b6732c230
andres-martin <andresmartin266@gmail.com>
2019-04-25 16:05:42 -0500

    comments feature implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/products', to: 'products#index'
   #get '/products/new', to: 'products#new', as: 'new_product'
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   #get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   #patch '/products/:id', to: 'products#update'
   #delete '/products/:id', to: 'products#destroy'
-  resources :products
+  resources :products do
+    resources :comments, only: [:create]
+  end  
   resources :users, only: [:new, :create]
   
 end
