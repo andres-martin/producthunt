e4b9125c7ef6d2f02fd64da63974beb260b3288b
andres-martin <andresmartin266@gmail.com>
2019-04-25 10:51:44 -0500

    sign out function created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,5 +11,11 @@ class SessionsController < ApplicationController
       render :new
     end  
   end
+
+  def destroy
+    sign_out 
+    redirect_to root_path
+  end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'products#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get '/products', to: 'products#index'
